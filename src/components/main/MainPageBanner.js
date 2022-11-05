b7dca15bbb177940fecc2359548c0548344fb0b4
Byeong Min Cho <merrybmc@gmail.com>
2022-11-06 00:18:53 +0900

    11/06/00:18 mainpage Layout update

--- src/components/main/MainPageBanner.js
+++ src/components/main/MainPageBanner.js
@@ -1,1 +1,20 @@
-export default function MainPageBanner() {}
+import styled from 'styled-components';
+
+export default function MainPageBanner() {
+  return (
+    <HeaderBannerContainer>
+      <HeaderBannerImg></HeaderBannerImg>
+    </HeaderBannerContainer>
+  );
+}
+
+const HeaderBannerContainer = styled.div`
+  width: 100%;
+  height: 700px;
+`;
+
+const HeaderBannerImg = styled.img`
+  width: 100%;
+  height: 772px;
+  background-color: orange;
+`;
