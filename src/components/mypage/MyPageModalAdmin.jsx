822244602d1e0af7ebf24faa9859cffcf883cf08
ksu930 <ksu930310@gmail.com>
2022-11-09 17:06:53 +0900

    11/09/17:06 관리자 승인 페이지 구현 완료 #46

--- src/components/mypage/MyPageModalAdmin.jsx
+++ src/components/mypage/MyPageModalAdmin.jsx
@@ -1,18 +1,11 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-export default function MyPageModal({
-  className,
-  setModalVisible,
-  maskClosable,
-  closable,
-  visible,
-  children,
-  onSubmitHandler,
-}) {
+export default function MyPageModalAdmin({ className, setModalVisible, maskClosable, closable, visible }) {
   const onMaskClick = (e) => {
     if (e.target === e.currentTarget) {
+      console.log('eee');
       setModalVisible(false);
     }
   };
@@ -23,29 +16,35 @@ export default function MyPageModal({
       <ModalWrapper className={className} onClick={maskClosable ? onMaskClick : null} tabIndex={-1} visible={visible}>
         <ModalInner tabIndex={0} className="modal-inner">
           {closable && (
-            <div className="buttons">
-              <button className="modal-close" type="button" onClick={() => setModalVisible(false)}>
-                취소
-              </button>
-              <button className="modal-subit" type="button" onClick={onSubmitHandler}>
-                완료
-              </button>
-            </div>
+            <>
+              <div className="buttons">
+                <button className="modal-close" type="button" onClick={() => setModalVisible(false)}>
+                  뒤로
+                </button>
+              </div>
+              <img src="https://yimgf-thinkzon.yesform.com/docimgs/public/1/34/33113/33112952.jpg" alt="img"></img>
+              <div className="admin-content">이름</div>
+              <div className="admin-content">이메일</div>
+              <div className="admin-content">신청일</div>
+              <div className="select-buttons">
+                <button>거절</button>
+                <button>승인</button>
+              </div>
+            </>
           )}
-          {children}
         </ModalInner>
       </ModalWrapper>
     </>
   );
 }
 
-MyPageModal.defaultProps = {
+MyPageModalAdmin.defaultProps = {
   visible: false,
   closable: true,
   maskClosable: true,
 };
 
-MyPageModal.propTypes = {
+MyPageModalAdmin.propTypes = {
   visible: PropTypes.bool,
 };
 
@@ -78,19 +77,37 @@ const ModalInner = styled.div`
   box-sizing: border-box;
   display: flex;
   flex-direction: column;
+  align-items: center;
   box-shadow: 0 0 6px 0 rgba(0, 0, 0, 0.5);
   background-color: #fff;
   border-radius: 10px;
   width: 480px;
   max-width: 480px;
-  height: 300px;
+  height: 600px;
   transform: translateY(-50%);
   margin: auto;
   margin-top: 400px;
   padding: 10px 20px;
+  gap: 30px;
   .buttons {
+    width: 100%;
     display: flex;
     flex-direction: row;
     justify-content: space-between;
+  }
+  img {
+    width: 80%;
+    height: 300px;
+  }
+  .admin-content {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+  }
+  .select-buttons {
+    display: flex;
+    flex-direction: row;
+    gap: 40px;
   }
 `;
--- src/router/Router.js
+++ src/router/Router.js
@@ -5,12 +5,14 @@ import RequestPage from '../pages/RequestPage';
 import MyPageConsultingList from '../pages/ConsultingList';
 import StanbyList from '../pages/StandbyList';
 import DoneList from '../pages/DoneList';
+import MypageAdmin from '../pages/MypageAdmin';
 
 export default function Router() {
   return (
     <>
       <Routes>
-        <Route path="/mypage" element={<MyPage />} />
+        {true ? <Route path="/mypage" element={<MypageAdmin />} /> : <Route path="/mypage" element={<MyPage />} />}
+        {/* userstate가 2(관리자)일 때 조건부 렌더링 */}
         <Route path="/" element={<MainPage />} />
         <Route path="/request" element={<RequestPage />} />
         <Route path="/consultinglist" element={<MyPageConsultingList />} />
