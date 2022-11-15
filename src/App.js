fd9e580280c2cdf79f4bd1b5e7e68cc76fe7e61c
superrookie8 <74133113+superrookie8@users.noreply.github.com>
2022-11-16 00:00:07 +0900

    회원가입,로그인 #59

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import Router from './router/Router';
+import { useRecoilState } from 'recoil';
+import { isLogin } from './store/store';
+import { useEffect } from 'react';
 
 function App() {
+  const [AppLogin, setAppLogin] = useRecoilState(isLogin);
+  useEffect(() => {
+    if (sessionStorage.getItem('access_token') !== null) {
+      setAppLogin(true);
+    } else {
+      setAppLogin(false);
+    }
+    console.log(AppLogin);
+  }, [AppLogin]);
+
   return (
     <div>
       <Router />
