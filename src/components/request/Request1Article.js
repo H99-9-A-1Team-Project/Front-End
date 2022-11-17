ecc29aab9b29b790fbc5a766580e546647621198
ksu930 <ksu930310@gmail.com>
2022-11-17 18:59:19 +0900

    11/17/18:58 컴포넌트 높이에따른 view 수정

--- src/components/request/Request1Article.js
+++ src/components/request/Request1Article.js
@@ -82,7 +82,7 @@ export default function Request1Article() {
 }
 
 const Rq1ArticleContainer = styled.div`
-  height: 684px;
+  height: 100%;
   background-color: white;
   display: flex;
   flex-direction: column;
@@ -174,8 +174,9 @@ const DetailAddressInput = styled.input`
 const NextBtn = styled.div`
   width: 328px;
   height: 60px;
-  margin-top: 400px;
   margin-left: 16px;
+  margin-top: auto;
+  margin-bottom: 32px;
   color: ${({ btnState }) => `${btnState === 0 ? 'black' : 'white'}`};
   background-color: ${({ btnState }) => `${btnState === 0 ? 'var(--gray5)' : 'var(--primary2-400)'}`};
   border-radius: 8px;
