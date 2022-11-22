002b67b4b134dd3e6e8810a4c92bba69e19aadd0
ksu930 <ksu930310@gmail.com>
2022-11-22 10:52:18 +0900

    11/22/10:51 중간저장 #104

--- src/components/MyPageMyConsult/ConsultDetailComment.js
+++ src/components/MyPageMyConsult/ConsultDetailComment.js
@@ -4,9 +4,10 @@ import deco from './sources/deco.png';
 import user_circle from '../MyPage/sources/userDefault.png';
 import good from './sources/Good.png';
 import good2 from './sources/Good2.png';
-import { Editor } from '@toast-ui/react-editor'; // Editor
+import { Editor, Viewer } from '@toast-ui/react-editor'; // Editor
 import '@toast-ui/editor/dist/toastui-editor.css'; // Editor css
 import '@toast-ui/editor/dist/i18n/ko-kr'; // Editor 한국어
+import '@toast-ui/editor/dist/toastui-editor-viewer.css';
 import { useRef } from 'react';
 import { useState } from 'react';
 
@@ -44,64 +45,66 @@ export default function ConsultDetailComment() {
     //     </div>
     //   </div>
     // </ConsultDetailCommentLayout>
-
-    // <ConsultDetailCommentLayout>
-    //   <div className="comment_body_wrap_answer">
-    //     <div className="header">username님의 답변입니다.</div>
-    //     <div className="realtor_info">
-    //       <div className="realtor_info_left">
-    //         <img src={user_circle} alt="user_circle" />
-    //       </div>
-    //       <div className="realtor_info_right">
-    //         <div className="realtor_info_right_top">공인중개사</div>
-    //         <div className="realtor_info_right_middle">username</div>
-    //         <div className="realtor_info_right_bottom">소개메세지소개메세지소개메세지소개메세지소개메세지소개메세지소개메세지소개메세지소개메세지소개메세지소개메세지소개메세지</div>
-    //       </div>
-    //     </div>
-    //     {/* <editorviwer></editorviwer> */}
-    //     {/* <div className="like">
-    //       <img src={good2} alt="good" />
-    //       답변이 많은 도움이 되었어요
-    //     </div>
-    //     <div className="dis_like">
-    //       <img src={good} alt="good" />
-    //       답변이 많은 도움이 되었어요
-    //     </div> */}
-    //   </div>
-    // </ConsultDetailCommentLayout>
-
-    <ConsultDetailCommentLayout>
-      <div className="editor_wrap">
-        <Editor
-          previewStyle={'tab'}
-          height="400px"
-          initialEditType="wysiwyg"
-          initialValue={'공인중개사님의 답변을 작성해 주세요.'}
-          hideModeSwitch={true}
-          usageStatistics={false}
-          language="ko-KR"
-          ref={editorRef}
-          onChange={onChange}
-          toolbarItems={[['image', 'link'], ['heading', 'bold'], ['hr'], ['ul', 'ol']]}
-          hooks={{
-            addImageBlobHook: async (blob, callback) => {
-              console.log(blob); // File {name: '카레유.png', ... }
-
-              // 1. 첨부된 이미지 파일을 서버로 전송후, 이미지 경로 url을 받아온다.
-              // const imgUrl = await .... 서버 전송 / 경로 수신 코드 ...
-
-              // 2. 첨부된 이미지를 화면에 표시(경로는 임의로 넣었다.)
-              callback('http://localhost:5000/img/카레유.png', '카레유');
-            },
-          }}
-        />
-        {/* <ToastUiViewer contents={contents} /> */}
-        <div className="buttons">
-          <button className="cancle_button">취소</button>
-          <button className="submit_button">답변 완료</button>
+    <>
+      <ConsultDetailCommentLayout>
+        <div className="comment_body_wrap_answer">
+          <div className="header">username님의 답변입니다.</div>
+          <div className="realtor_info">
+            <div className="realtor_info_left">
+              <img src={user_circle} alt="user_circle" />
+            </div>
+            <div className="realtor_info_right">
+              <div className="realtor_info_right_top">공인중개사</div>
+              <div className="realtor_info_right_middle">username</div>
+              <div className="realtor_info_right_bottom">소개메세지소개메세지소개메세지소개메세지소개메세지소개메세지소개메세지소개메세지소개메세지소개메세지소개메세지소개메세지</div>
+            </div>
+          </div>
+          <div className="editor_viwer_wrap">
+            <Viewer initialValue="<h3> html 헤더 <span >파란색</span></h3>" />
+          </div>
+          {/* <div className="like">
+          <img src={good2} alt="good" />
+          답변이 많은 도움이 되었어요
         </div>
-      </div>
-    </ConsultDetailCommentLayout>
+        <div className="dis_like">
+          <img src={good} alt="good" />
+          답변이 많은 도움이 되었어요
+        </div> */}
+        </div>
+      </ConsultDetailCommentLayout>
+
+      <ConsultDetailCommentLayout>
+        <div className="editor_wrap">
+          <Editor
+            previewStyle={'tab'}
+            height="400px"
+            initialEditType="wysiwyg"
+            initialValue={'공인중개사님의 답변을 작성해 주세요.'}
+            hideModeSwitch={true}
+            usageStatistics={false}
+            language="ko-KR"
+            ref={editorRef}
+            onChange={onChange}
+            toolbarItems={[['image', 'link'], ['heading', 'bold'], ['hr'], ['ul', 'ol']]}
+            hooks={{
+              addImageBlobHook: async (blob, callback) => {
+                console.log(blob); // File {name: '카레유.png', ... }
+
+                // 1. 첨부된 이미지 파일을 서버로 전송후, 이미지 경로 url을 받아온다.
+                // const imgUrl = await .... 서버 전송 / 경로 수신 코드 ...
+
+                // 2. 첨부된 이미지를 화면에 표시(경로는 임의로 넣었다.)
+                callback('http://localhost:5000/img/카레유.png', '카레유');
+              },
+            }}
+          />
+          <div className="buttons">
+            <button className="cancle_button">취소</button>
+            <button className="submit_button">답변 완료</button>
+          </div>
+        </div>
+      </ConsultDetailCommentLayout>
+    </>
   );
 }
 const ConsultDetailCommentLayout = styled.div`
