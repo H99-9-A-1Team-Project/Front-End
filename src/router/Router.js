4ca6ab12805658406259c6e40d0f0612c479a253
Byeong Min Cho <merrybmc@gmail.com>
2022-11-29 10:06:30 +0900

    11/29/10:06 default design #125

--- src/router/Router.js
+++ src/router/Router.js
@@ -6,7 +6,6 @@ import Request from '../pages/RequestMain';
 import Request1 from '../pages/Request1';
 import Request2 from '../pages/Request2';
 import Request3 from '../pages/Request3';
-import './route.css';
 import RequestCheck from '../pages/RequestCheck';
 import MyPageMyConsult from '../pages/MyPageMyConsult';
 import MyPageAdmin from '../pages/MyPageAdmin';
@@ -17,12 +16,16 @@ import MyPageRealtorConsult from '../pages/MyPageRealtorConsult';
 import FootStepDetail from '../pages/FootStepDetail';
 import IntroducePage from '../pages/IntroducePage';
 import MainPageSideBar from '../components/main/MainPageSideBar';
+import styled from 'styled-components';
+import DefaultLeft from './DefaultLeft';
+import DefaultRight from './DefaultRight';
+
 export default function Router() {
   return (
     <>
-      <div className="container">
-        <div className="default"></div>
-        <div className="app">
+      <Container>
+        <DefaultLeft />
+        <App>
           <Routes>
             <Route path="/" element={<MainPage />} />
             <Route path="/signup" element={<SignUp />} />
@@ -44,9 +47,61 @@ export default function Router() {
             <Route path="/introduce" element={<IntroducePage />} />
           </Routes>
           <MainPageSideBar />
-        </div>
-        <div className="default"></div>
-      </div>
+        </App>
+        <DefaultRight />
+      </Container>
     </>
   );
 }
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const App = styled.div`
+  width: 360px;
+  min-height: 100vh;
+  max-height: fit-content;
+  display: flex;
+  flex-direction: row;
+  background-color: white;
+  box-shadow: 0px 8px 12px 6px rgba(0, 0, 0, 0.1), 0px 4px 4px rgba(0, 0, 0, 0.1);
+  z-index: 1;
+`;
+
+const Backgrounds = styled.div`
+  background-color: #edf0f3;
+  width: 100%;
+`;
+
+const LeftHeadline = styled.div`
+  font-family: var(--headline-font-family);
+  color: var(--gray1);
+  margin-left: 120px;
+  margin-top: 331px;
+  cursor: default;
+  font-size: 32px;
+  line-height: 48.9px;
+`;
+
+const LogoImg = styled.img`
+  margin-left: 112px;
+  margin-top: 12px;
+  width: 167px;
+  height: 40.02px;
+`;
+
+const LeftHome1 = styled.img`
+  position: absolute;
+  margin-top: 341.24px;
+  margin-left: 180px;
+  z-index: 1;
+  opacity: 0.66;
+`;
+const LeftHome2 = styled.img`
+  position: absolute;
+  margin-left: 454px;
+  margin-top: 255px;
+  z-index: 0;
+  opacity: 0.6;
+`;
