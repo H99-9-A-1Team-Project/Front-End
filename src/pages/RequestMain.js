7e5df981442c20c153d1d7d4cfd24db78f2903b2
ksu930 <ksu930310@gmail.com>
2022-12-09 23:46:46 +0900

    12/09/23:24 비로그인 상태로 매물상담 페이지 접근하는 경우 메인페이지로 리다이렉트

--- src/pages/RequestMain.js
+++ src/pages/RequestMain.js
@@ -4,8 +4,19 @@ import RequestArticle from '../components/requestmain/RequestMainArticle';
 import RequestList from '../components/requestmain/RequestMainList';
 import MainPageTabBar from '../components/main/MainPageTabBar';
 import Layout from '../global/components/Layout';
+import { useRecoilValue } from 'recoil';
+import { isLogin } from '../store/store';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function Request() {
+  const LoginState = useRecoilValue(isLogin);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (LoginState === false) {
+      navigate('/');
+    }
+  });
   return (
     <Layout>
       <MainPageHeader />
